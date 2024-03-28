4a201c36be79b06d8aab1e419837f80a49eaea80
marcusvcmaia <148342981+marcusvcmaia@users.noreply.github.com>
2024-03-28 19:48:17 -0300

    Update and rename cria pilha vazia.c to pilha e fila.c

--- cria pilha vazia.c
+++
@@ -1,7 +0,0 @@
-pilha *p = cria_pilha();
-
-{
-pilha *p = malloc (sizeof(pilha);
-p->topo=NULL;
-return p;
-}
---
+++ pilha e fila.c
@@ -0,0 +1,23 @@
+//CRIA PILHA VAZIA
+
+pilha *p = cria_pilha();
+
+{
+pilha *p = malloc (sizeof(pilha);  //aloca espaco para a pilha
+p->topo=NULL;                      // testa pilha vazia
+return p;                          // retorna valor
+}
+
+
+// CRIA TIPO PILHA
+
+typedef struct fila {             // cria struc para fila
+no *inicio;
+no *fim;            
+}fila;
+
+// INSERIR NA FILA
+
+
+//CRIA FILA VAZIA
+pilha *p = malloc (sizeof(pilha); 
