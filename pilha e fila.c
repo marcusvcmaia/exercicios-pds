d2e0f34c1a2a681a341c83d7e9339e141a2af279
marcusvcmaia <148342981+marcusvcmaia@users.noreply.github.com>
2024-04-03 19:22:46 -0300

    Update pilha e fila.c

--- pilha e fila.c
+++ pilha e fila.c
@@ -21,3 +21,34 @@ no *fim;
 
 //CRIA FILA VAZIA
 pilha *p = malloc (sizeof(pilha); 
+p->inicio=NULL
+p->fim=NULL
+
+
+/////////
+typedef struct fila {
+struct fila *inicio;
+struct fila *fim;
+}fila;
+
+typedef struct no{    
+int valor;
+struct no *proximo;
+}no;
+
+malloc (sizeof(fila);
+
+
+if {
+fila -> inicio ==NULL}
+fila -> inicio = no;
+
+no-> proximo = NULL;
+if {
+fila -> fim != NULL}
+fila ->fim->proximo=no;
+fila -> fim = no;
+
+  
+  
+  
